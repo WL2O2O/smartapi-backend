c59132ab5c78eaa4f273e156b446538e84ae91ba
WLei224 <1149630057@qq.com>
2023-07-04 22:15:37 +0800

    first commit

---
+++ src/main/java/com/yupi/project/service/InterfaceInfoService.java
@@ -0,0 +1,19 @@
+package com.yupi.project.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.project.model.entity.InterfaceInfo;
+import com.yupi.project.model.entity.InterfaceInfo;
+
+/**
+ *
+ */
+public interface InterfaceInfoService extends IService<InterfaceInfo> {
+
+    /**
+     * 校验
+     *
+     * @param interfaceInfo
+     * @param add 是否为创建校验
+     */
+    void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean add);
+}
