ffa3bbee17f85e41014ff7385e7360a692fccf11
WLei224 <1149630057@qq.com>
2023-07-13 18:43:37 +0800

    add 管理接口、申请签名、模拟接口调用

--- src/main/java/com/yupi/project/service/impl/UserServiceImpl.java
+++ src/main/java/com/yupi/project/service/impl/UserServiceImpl.java
@@ -164,6 +164,14 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         return true;
     }
 
+    class Solution {
+        public String addBinary(String a, String b) {
+            return Integer.toBinaryString(Integer.parseInt(a,2) + Integer.parseInt(b,2));
+
+
+        }
+    }
+
 }
 
 
